c9de8b8d932e8364e0e049583635a4042f56eebb
Gabriel Silveira <gabriel@gabrielsilveira.com.br>
2022-08-20 19:35:51 +0100

    first commit

---
+++ src/features/pages/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="absolute top-0 w-screen py-3 px-5 bg-slate-100">
+      <b>Gabriel Silveira</b> | Front-end Developer
+    </header>
+  )
+}
+
+export default Header
